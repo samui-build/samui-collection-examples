635eb08f1ca43d17d1a5c92ec952430f7ff20c55
Bram Borggreve <beeman@beeman.nl>
2025-02-02 18:36:36 +0000

    feat: show collection and asset images

--- generate.ts
+++ generate.ts
@@ -30,17 +30,25 @@ async function main() {
   await $`rm -rf ${outputDir}`
   await $`mkdir -p ${outputDir}`
 
+  const collectionDetails: { id: string; name: string; description: string }[] = []
+
   for (const collectionId of collections) {
     const collectionUrl = `${baseUrl}/${collectionId}`
     const collectionAssetsPath = `${collectionId}/assets`
     const outputAssetsPath = `${outputDir}${collectionAssetsPath}`
-    const collectionLinks: string[] = [`./assets/collection.json`]
+
     const tag = `[${collectionId}]`
     console.log(`${tag} => Generating collection`)
 
     await $`mkdir -p ${outputDir}${collectionId}/assets`
 
     const { assetFiles, collectionJson } = await getCollectionContext(collectionId)
+    console.log(`${tag} => Collection Details of ${collectionJson.name}`)
+    collectionDetails.push({
+      id: collectionId,
+      name: collectionJson.name,
+      description: collectionJson.description,
+    })
 
     // Copy the collection.png file to the output directory
     console.log(`${tag}   => Collection.png Copying...`)
@@ -56,7 +64,6 @@ async function main() {
     console.log(`${tag} => Assets...`)
 
     for (const file of assetFiles) {
-      collectionLinks.push(`assets/${file}`)
       const fileContent = await readFile(`${collectionAssetsPath}/${file}`, 'utf-8')
       if (!fileContent) {
         console.log(`${tag}\t => Skipping ${file}`)
@@ -76,14 +83,15 @@ async function main() {
         fileContent.replaceAll(image, `${collectionUrl}/assets/${image}`),
       )
     }
-    // write collection index file
-    console.log(`${tag} => Writing index.html`)
+    // write gallery index file
+    console.log(`${tag} => Writing gallery.html`)
     await writeFile(
       `${outputDir}${collectionId}/index.html`,
-      createHtmlPage({
+      createCollectionPage({
+        id: collectionId,
         title: collectionJson.name,
         description: collectionJson.description,
-        links: collectionLinks,
+        assets: assetFiles,
       }),
     )
   }
@@ -94,7 +102,7 @@ async function main() {
     createHtmlPage({
       title: 'Samui Collection Examples',
       description: 'This is a collection of Samui Collection Examples.',
-      links: collections.map((collectionId) => `${collectionId}/`),
+      collections: collectionDetails,
     }),
   )
 }
@@ -103,7 +111,15 @@ main().catch((error) => {
   throw error
 })
 
-function createHtmlPage({ description, links, title }: { description?: string; links: string[]; title?: string }) {
+function createHtmlPage({
+  collections,
+  description,
+  title,
+}: {
+  collections: { id: string; name: string; description: string }[]
+  description?: string
+  title?: string
+}) {
   return `
     <!DOCTYPE html>
     <html>
@@ -116,10 +132,91 @@ function createHtmlPage({ description, links, title }: { description?: string; l
       <body>
         <h1>${title}</h1>
         <p>${description}</p>
-        <ul>
-          ${links.map((item) => `<li><a href="${item}">${item.replace('./', '').replace(/\/$/, '')}</a></li>`).join('')}
-        </ul>
+        <div style="display: flex; flex-direction: column; gap: 24px">
+          ${collections
+            .map((item) =>
+              createCollectionCard({
+                description: item.description,
+                title: item.name,
+                path: item.id,
+                id: '',
+              }),
+            )
+            .join('')}
+        </div>
       </body>
     </html>
+  `.trim()
+}
+
+function createCollectionPage({
+  description,
+  id,
+  assets,
+  title,
+}: {
+  description: string
+  id: string
+  assets: string[]
+  title: string
+}) {
+  return `
+    <!DOCTYPE html>
+    <html>
+    <head>
+      <title>${title}</title>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/water.css@2/out/water.css">
+      </head>
+      <body>
+        <div style="display: flex; flex-direction: column; gap: 24px">
+          ${createCollectionCard({ description, id, title, path: '.' })}
+          <div style="display: grid; grid-template-columns: repeat(auto-fit, minmax(250px, 1fr)); gap: 12px;">
+            ${assets.map((json) => createAssetCard({ json, path: '.' })).join('')}
+          </div>
+        </div>
+      </body>
+    </html>
+  `.trim()
+}
+
+export function createCollectionCard({
+  description,
+  id,
+  title,
+  path = '.',
+}: {
+  description: string
+  id: string
+  title: string
+  path: string
+}) {
+  return `
+    <div style="display: flex; flex-direction: column; gap: 24px">        
+      <div style="display: flex; align-items: center; gap: 24px;">
+        <img src="${path}/assets/collection.png" alt="collection.png" style="max-height: 256px;">
+        <div>
+          <h1><a href="/${path}${id ? `/${id}` : ''}/">${title}</a></h1>      
+          <p>${description}</p>
+          <span>
+            <a href="${path}/assets/collection.json">collection.json</a>
+            <a href="${path}/assets/collection.png">collection.png</a>
+          </span>
+        </div>
+    </div>
+  `.trim()
+}
+
+export function createAssetCard({ json, path }: { json: string; path: string }) {
+  const image = json.replace('json', 'png')
+  return `
+    <div style="text-align: center;">
+      <img src="${`${path}/assets/` + image}" alt="${image}" style="max-height: 256px;">
+      <span>
+        <a href="${path}/assets/${json}">${json}</a>
+        <a href="${path}/assets/${image}">${image}</a>
+      </span>
+    </div>
   `.trim()
 }
